38f7d037451b57096e0bfa8aa4485ae756d20f42
AndresTs2 <hector.bojorge.c@uniautonoma.edu.co>
2021-09-24 14:22:30 -0500

    actualizacion estaciones

--- ProyectoCiclo3.App.Frontend/Pages/Estaciones/Create.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Estaciones/Create.cshtml.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
  
-namespace HolaWeb.App.Frontend.Pages
+namespace  ProyectoCiclo3.App.Frontend.Pages
 {
     public class FormEstacionesModel : PageModel
     {
